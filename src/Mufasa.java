b56b10344666909065108da9b5c488276d2e9fc8
Sakari Järvelä <sjarvela89@hotmail.com>
2018-10-22 20:49:52 +0300

    Assignment finished.

--- src/Mufasa.java
+++ src/Mufasa.java
@@ -1,4 +1,80 @@
-
+import java.util.*;
+import java.io.*;
 public class Mufasa {
-
+	String firstName;
+	String lastName;
+	String password;
+	String country;
+	String birthday;
+	String phonenumber;
+	String email;
+	private Scanner scanString;
+	public void setFirstName()
+	{
+		scanString = new Scanner(System.in);
+		firstName=scanString.next();
+	}
+	public void setLastName()
+	{
+		lastName=scanString.next();
+	}
+	public void setPwd()
+	{
+		boolean flag=false;
+		password=scanString.next();
+		flag=reEnterPwd(password);
+		if(flag==true)
+		{
+			writeDB();
+		}
+	}
+	private boolean reEnterPwd(String firstPWD)
+	{
+		String secondPWD=scanString.next();
+		if(firstPWD==secondPWD)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+		
+	}
+	public void selectCountry()
+	{
+		
+	}
+	public void setBDay(String dd, String mm, String yyyy)
+	{
+		
+	}
+	public void setPNBR()
+	{
+		
+	}
+	public void setEmail(String emailSet)
+	{
+		
+	}
+	private void checkEmail()
+	{
+		
+	}
+	public Double getUserInfo()
+	{
+		
+	}
+	public Double secondDouble()
+	{
+		
+	}
+	private void writeDB()
+	{
+		
+	}
+	public void readDB(userDB db)
+	{
+		
+	}
 }
--- tests/Person.java
+++ tests/Person.java
@@ -1,12 +1,26 @@
 import static org.junit.Assert.*;
 
+import org.easymock.EasyMock;
+import org.junit.Before;
 import org.junit.Test;
+
+import fi.oulu.tol.sqat.StockMarket;
 
 public class Person {
 
+	Mufasa testMuf;
+	userDB testDB;
+	@Before
+	public void setup(){
+		testMuf=new Mufasa();
+		testDB=EasyMock.createMock(userDB.class);
+		testMuf.readDB(testDB);
+	}
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		EasyMock.expect(testDB.getUserInfo("Sakari")).andReturn("Sakari Järvelä");
+		EasyMock.replay(testDB);
+		Mufasa sakari = new Mufasa("Sakari",1);
 	}
 
 }
